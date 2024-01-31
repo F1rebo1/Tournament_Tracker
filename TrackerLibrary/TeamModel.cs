fcfafb9d99fa909792ca88a528885fa9111f159b
ROHAN SHANBHAG <shanb020@umn.edu>
2024-01-31 11:46:26 -0600

    Added ClassLibrary, PersonModel, TeamModel and TournamentModel classes

---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TrackerLibrary{
+	public class TeamModel{
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        public string TeamName {get; set;};
+    }
+}
