d69f2cef0b64df7bc96efff6f4420e59f09f76c1
Rohan Shanbhag <rohanshanbhag2001@gmail.com>
2024-02-13 23:58:56 -0600

    Tested Textfile and SQL connections for CreateTeamForm.cs

--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -14,7 +14,7 @@ namespace TrackerUI
 
             // Initialize the database connections
             //TrackerLibrary.GlobalConfig.InitializeConections(TrackerLibrary.DatabaseType.Sql);
-            TrackerLibrary.GlobalConfig.InitializeConections(TrackerLibrary.DatabaseType.TextFile);
+            TrackerLibrary.GlobalConfig.InitializeConections(TrackerLibrary.DatabaseType.Sql);
 
             Application.Run(new CreateTeamForm());
             //Application.Run(new TournamentDashboardForm());
